e6a02a2adb295fedd4d29b8e02c8bf282e4823cd
Quintis1212 <34488718+Quintis1212@users.noreply.github.com>
2023-05-13 11:08:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from "react-router-dom";
 console.log({})
+(()=>{
+console.log({})
+})()
+(()=>{
+console.log({})
+})()
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
